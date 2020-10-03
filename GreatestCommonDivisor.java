f52c75ccf812fc6ef11848c86580c560f19fb30c
Jalal Seidi <jalalseidi7@gmail.com>
2020-10-03 15:08:00 +0330

    Add GreatestCommonDivisor problem V2

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -2,17 +2,31 @@ public class GreatestCommonDivisor {
     public static void main(String[] args) {
         System.out.println(getGreatestCommonDivisor(10, 35));
     }
-    public static int getGreatestCommonDivisor(int first, int second) {
-        int greatestdevisor = 0;
-        if (first >= 10 && second >= 10) {
-            for (int i = 1; i <= first && i <= second; i++) {
-                if (first % i == 0 && second % i == 0)
-                    greatestdevisor = i;
-            }
-            return greatestdevisor;
 
+    public static int getGreatestCommonDivisor(int first, int second) {
+        int greatestdivisor = 0;
+        if (first >= 10 && second >= 10) {
+            if (first < second) {
+                for (int i = 1; i <= first; i++) {
+                    if (first % i == 0 && second % i == 0)
+                        greatestdivisor = i;
+                }
+            }
+            if (second < first) {
+                for (int i = 1; i <= second; i++) {
+                    if (second % i == 0 && first % i == 0)
+                        greatestdivisor = i;
+                }
+            }
+            if (first == second) {
+                for (int i = 1; i <= second; i++) {
+                    if (second % i == 0)
+                        greatestdivisor = i;
+                }
+            }
         } else {
             return -1;
         }
+        return greatestdivisor;
     }
 }
