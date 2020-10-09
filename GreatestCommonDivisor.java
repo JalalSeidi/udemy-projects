19e0698e15b2e2f7bfd15a67754c585b01ddc1db
Jalal Seidi <jalalseidi7@gmail.com>
2020-10-10 00:25:51 +0330

    GreatestCommonDivisor problem V3

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -5,24 +5,11 @@ public class GreatestCommonDivisor {
 
     public static int getGreatestCommonDivisor(int first, int second) {
         int greatestdivisor = 0;
+        int min = Math.min(first, second);
         if (first >= 10 && second >= 10) {
-            if (first < second) {
-                for (int i = 1; i <= first; i++) {
-                    if (first % i == 0 && second % i == 0)
-                        greatestdivisor = i;
-                }
-            }
-            if (second < first) {
-                for (int i = 1; i <= second; i++) {
-                    if (second % i == 0 && first % i == 0)
-                        greatestdivisor = i;
-                }
-            }
-            if (first == second) {
-                for (int i = 1; i <= second; i++) {
-                    if (second % i == 0)
-                        greatestdivisor = i;
-                }
+            for (int i = 1; i <= min ; i++) {
+                if (first % i == 0 && second % i == 0)
+                    greatestdivisor = i;
             }
         } else {
             return -1;
