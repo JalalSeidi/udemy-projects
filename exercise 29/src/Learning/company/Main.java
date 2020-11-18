3eca9879503b946a729c365a460b27967b903be7
Jalal Seidi <jalalseidi7@gmail.com>
2020-11-18 15:23:50 +0330

    Simple calculator problem

--- exercise 29/src/Learning/company/Main.java
+++ exercise 29/src/Learning/company/Main.java
@@ -3,5 +3,14 @@ package Learning.company;
 public class Main {
 
     public static void main(String[] args) {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        simpleCalculator.setFirstNumber(5.0);
+        simpleCalculator.setSecondNumber(4);
+        System.out.println("add = " + simpleCalculator.getAdditionResul());
+        System.out.println("subtract = " + simpleCalculator.getSubtractionResult());
+        simpleCalculator.setFirstNumber(5.25);
+        simpleCalculator.setSecondNumber(0);
+        System.out.println("multiply = " + simpleCalculator.getMultiplicationResult());
+        System.out.println("divide = " + simpleCalculator.getDivisionResult());
     }
 }
