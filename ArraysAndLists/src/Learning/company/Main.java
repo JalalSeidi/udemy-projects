cd6bb9a0666dde9f566fdf6785ae871e1b1a8b07
Jalal Seidi <jalalseidi7@gmail.com>
2021-01-30 22:02:50 +0330

    ReverseArray

--- ArraysAndLists/src/Learning/company/Main.java
+++ ArraysAndLists/src/Learning/company/Main.java
@@ -1,5 +1,6 @@
 package Learning.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,10 +16,17 @@ public class Main {
 //        sortedArray.printArray(sorted);
 
 //      Exercise 42
-        MinimumElement minimumElement = new MinimumElement();
-        int count = minimumElement.readIntegers();
-        int[] array = minimumElement.readElements(count);
-        int minimum = minimumElement.findMin(array);
-        System.out.println("min = " + minimum);
+//        MinimumElement minimumElement = new MinimumElement();
+//        int count = minimumElement.readIntegers();
+//        int[] array = minimumElement.readElements(count);
+//        int minimum = minimumElement.findMin(array);
+//        System.out.println("min = " + minimum);
+
+//      Exercise 43
+        ReverseArray reverseArray = new ReverseArray();
+        int[] array = { 2, 3, 4, 6 ,8 ,9 ,10};
+        System.out.println("Array = " + Arrays.toString(array));
+        ReverseArray.reverse(array);
+        System.out.println("Reversed array = " + Arrays.toString(array));
     }
 }
---
+++ ArraysAndLists/src/Learning/company/ReverseArray.java
@@ -0,0 +1,19 @@
+package Learning.company;
+
+public class ReverseArray {
+
+    /**
+     * Reverses the elements in the array
+     * @param array array of int
+     */
+    public static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+        for (int i = 0; i < halfLength; i++) {
+            int temporarily = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temporarily;
+        }
+    }
+}
+
