d7e7347fda76c99b8e5bbe3e95c3f004197a3818
Jalal Seidi <jalalseidi7@gmail.com>
2021-02-07 19:08:21 +0330

    Banking

--- ArraysAndLists/src/Learning/company/Main.java
+++ ArraysAndLists/src/Learning/company/Main.java
@@ -30,16 +30,30 @@ public class Main {
 //        System.out.println("Reversed array = " + Arrays.toString(array));
 
 //      Exercise 44
-          Contact contact1 = new Contact("jalal", "09214017053");
-          Contact contact2 = new Contact("Nahid", "0045654");
-          MobilePhone mobilePhone = new MobilePhone("12");
-          mobilePhone.addNewContact(contact1);
-          mobilePhone.addNewContact(contact2);
-          mobilePhone.printContacts();
-          mobilePhone.removeContact(contact2);
-          mobilePhone.printContacts();
+//          Contact contact1 = new Contact("jalal", "09214017053");
+//          Contact contact2 = new Contact("Nahid", "0045654");
+//          MobilePhone mobilePhone = new MobilePhone("12");
+//          mobilePhone.addNewContact(contact1);
+//          mobilePhone.addNewContact(contact2);
+//          mobilePhone.printContacts();
+//          mobilePhone.removeContact(contact2);
+//          mobilePhone.printContacts();
+
+//        Exercise 45
+        Bank bank = new Bank("National Australia Bank");
+
+        bank.addBranch("Adelaide");
+
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Mike", 175.34);
+        bank.addCustomer("Adelaide", "Percy", 220.12);
 
 
-        
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+        bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
+
+        bank.listCustomers("Adelaide", true);
+
     }
 }
