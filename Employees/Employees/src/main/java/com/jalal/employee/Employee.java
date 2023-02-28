8c4a087539d1ff007d9d80606b26be860b9949d2
jalalSeidi <jalalseidi7@gmail.com>
2023-02-28 22:57:52 +0000

    using the superclass in the Employee class

--- Employees/Employees/src/main/java/com/jalal/employee/Employee.java
+++ Employees/Employees/src/main/java/com/jalal/employee/Employee.java
@@ -9,20 +9,31 @@ import java.util.regex.Pattern;
 public abstract class Employee {
   protected final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
   protected final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
-  private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}), \\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
-  protected final Pattern peoplePat = Pattern.compile(peopleRegex);
+  private static final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}), \\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
+  protected static final Pattern peoplePat = Pattern.compile(peopleRegex);
   protected final Matcher peopleMat;
   protected String lastName;
   protected String firstName;
   protected LocalDate dob;
 
   public Employee(String personText) {
-    peopleMat = peoplePat.matcher(personText);
+    peopleMat = Employee.peoplePat.matcher(personText);
     if (peopleMat.find()) {
       this.lastName = peopleMat.group("lastName");
       this.firstName = peopleMat.group("firstName");
       this.dob = LocalDate.from(dtFormatter.parse(peopleMat.group("dob")));
     }
+  }
+
+  public static final Employee createEmployee(String employeeText) {
+    Matcher peopleMat = Employee.peoplePat.matcher(employeeText);
+    return switch (peopleMat.group("role")) {
+      case "Programmer" -> new Programmer(peopleMat.group());
+      case "Manger" -> new Manager(peopleMat.group());
+      case "Analyst" -> new Analyst(peopleMat.group());
+      case "CEO" -> new CEO(peopleMat.group());
+      default -> null;
+    };
   }
 
   public abstract int getSalary();
