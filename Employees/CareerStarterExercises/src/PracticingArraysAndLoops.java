166dda398487b8a39f027c6a78e870a38818cb6e
jalalSeidi <jalalseidi7@gmail.com>
2023-05-05 23:01:56 +0100

    Performing a better way of Switch

--- Employees/CareerStarterExercises/src/PracticingArraysAndLoops.java
+++ Employees/CareerStarterExercises/src/PracticingArraysAndLoops.java
@@ -1,31 +1,19 @@
+
 public class PracticingArraysAndLoops {
 
   public static void main(String[] args) {
     String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     for (String day : days) {
-      switch (day) {
-        case "Sunday":
-        System.out.println("We eat pot roast on Sunday");
-        break;
-        case "Monday":
-        System.out.println("We eat spaghetti on Monday");
-        break;
-        case "Tuesday":
-        System.out.println("We eat tacos on Tuesday");
-        break;
-        case "Wednesday":
-        System.out.println("We eat chicken on Wednesday");
-        break;
-        case "Thursday":
-        System.out.println("We eat meatloaf on Thursday");
-        break;
-        case "Friday":
-        System.out.println("We eat hamburgers on Friday");
-        break;
-        default: // technically, no need for another "else if" since there's only one remaining option
-        System.out.println("We eat pizza on Saturday");
-        break;
-      }
+      String meal = switch (day) {
+        case "Sunday" -> "pot roast";
+        case "Monday" -> "spagheti";
+        case "Tuesday" -> "tacos";
+        case "Wednesday" -> "chicken";
+        case "Thursday" -> "meatloaf";
+        case "Friday" -> "hamburgers";
+        default -> "pizza";
+      };
+      System.out.printf("We eat %s on %s%n", meal, day);
     }
   }
 }
