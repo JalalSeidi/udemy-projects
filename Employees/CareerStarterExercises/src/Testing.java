f70ce56a7771741073ff26553fe421424a07e28d
jalalSeidi <jalalseidi7@gmail.com>
2023-05-07 23:30:49 +0100

    Practicing testing in Java

---
+++ Employees/CareerStarterExercises/src/Testing.java
@@ -0,0 +1,19 @@
+public class Testing {
+
+  public static void main(String[] args) {
+    System.out.println(alternateCaps("roses"));
+  }
+
+  public static String alternateCaps(String word) {
+    StringBuilder sb = new StringBuilder(word.length());
+    for (int index=0; index<word.length(); index++) {
+      String curLetter = word.substring(index, index+1);
+      if (index % 2 == 0) {
+        sb.append(curLetter);
+      } else {
+        sb.append(curLetter.toUpperCase());
+      }
+    }
+    return sb.toString();
+  }
+}
